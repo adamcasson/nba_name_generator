388bc6d9e54403d87a2acec036b912897b737f8a
adamcasson <adamcasson@yahoo.com>
2020-05-05 01:17:41 -0400

    Refactored into class

--- name_generator.py
+++ name_generator.py
@@ -6,8 +6,11 @@ Created on Sat Apr 13 10:19:37 2019
 @author: adam
 """
 
+from typing import List
+
 import numpy as np
 from basketball_reference_web_scraper import client
+
 
 def get_player_names(season_end_years=[2019]):
     """Gets a list of player first names and last names for the
@@ -42,143 +45,13 @@ def get_player_names(season_end_years=[2019]):
         if name not in player_names:
             player_names.append(name)
     
-    # Separate the first and last names into their own datasets
+    # Separate the first and last names into their own lists
     # and lower case them for consistency
     first_names = [x[0].lower() for x in player_names]
     last_names = [x[1].lower() for x in player_names]
 
     return first_names, last_names
 
-def make_vocab(names):
-    """Creates a list of unique characters seen in the given names
-        
-    Args:
-        names (list): A list of strings of names to create vocab
-            
-    Returns:
-        list: Unique characters from names
-    
-    """
-    
-    # Initialize list with our designated start and end tokens
-    vocab = ['<','>']
-    
-    # Collect unique characeters
-    for name in names:
-        for char in list(name):
-            if char not in vocab:
-                vocab.append(char)
-    return vocab
-
-def new_freq_matrix(size):
-    """Creates an empty numpy array used for holding frequency
-        statistics between each character
-        
-    Args:
-        size (int): Size of matrix, i.e. number of unique characters
-            
-    Returns:
-        ndarray: Zero initialized array of shape (size, size)
-    
-    """
-    freq_matrix = np.zeros((size, size))
-    return freq_matrix
-
-def transition_probabilities(names, vocab):
-    """Calculates and stores the empirical transition probabilites 
-        for every character at every time step in the given dataset
-        
-    Args:
-        names (list): A list of strings of names to compute
-            probabilities from
-        vocab (list): A list of unique characters in names
-            
-    Returns:
-        dictionary: Transition probabilities array at each time step
-    
-    """
-    
-    # Specify our start and end tokens
-    start = '<'
-    end = '>'
-    
-    # Get size of the vocab
-    size = len(vocab)
-    transitions = {}
-    
-    for name in names:
-        # Append the start and end tokens to the name string in order
-        # to easily include them in calculations
-        name = start+name+end
-        
-        for i, char in enumerate(name):
-            # We iterate until the end token, since no transition can
-            # be made at the end token
-            if i+1 < len(name):
-                freq_matrix = transitions.get(str(i), new_freq_matrix(size))
-                state = vocab.index(char)
-                next_state = vocab.index(name[i+1])
-                freq_matrix[state, next_state] += 1
-                transitions[str(i)] = freq_matrix
-                
-    for i in transitions.keys():
-        freq_matrix = transitions[i]
-        freq_matrix = np.nan_to_num(freq_matrix/freq_matrix.sum(axis=1)[:,None])
-        transitions[i] = freq_matrix
-                
-    return transitions
-
-def generate_name(transitions, vocab, random=True):
-    """Generate a name give then transition probabilities and
-        unique characters
-        
-    Args:
-        transitions (dict): Transition probabilities array at each time
-            step
-        vocab (list): A list of unique characters in names
-        random (bool, optional): If True then each character will be
-            randomly sampled based on the previous character. If False
-            then the name generation will be deterministic by choosing the
-            most frequent letter at each time step based on the last letter
-            
-    Returns:
-        str: The generated name
-    
-    """
-    
-    # Initialize name with the designated start token
-    name = '<'
-    current_char = '<'
-    
-    for i in transitions.keys():
-        # Get the transition probabilities for the current time step
-        prob_matrix = transitions[str(i)]
-        
-        # Get the position of the current character
-        state = vocab.index(current_char)
-        
-        # Get the row of transition probs for the current character
-        prob_row = prob_matrix[state]
-        
-        if random:
-            # Randomly sample from P(x|c) to choose the next character
-            new_state = np.random.choice(len(vocab), p=prob_row)
-        else:
-            # Or choose the character with highest frequency
-            new_state = np.argmax(prob_row)
-        
-        # Get the new character string based on the position
-        new_char = vocab[new_state]
-        # Append to the name
-        name += new_char
-        # Reassign current character for next iteration
-        current_char = new_char
-        # Break once the end token is chosen
-        if current_char == '>':
-            break
-        
-    # Strip the name of the start and end tokens
-    return name.strip('<>').capitalize()
 
 def letter_error_rate(fake_name, real_name):
     d = edit_distance(fake_name, real_name)
@@ -186,6 +59,7 @@ def letter_error_rate(fake_name, real_name):
     
     return result
     
+
 def edit_distance(fake_name, real_name):
     d = np.zeros((len(fake_name)+1, len(real_name)+1), dtype=np.uint8)
     for i in range(len(fake_name)+1):
@@ -205,6 +79,7 @@ def edit_distance(fake_name, real_name):
                 d[i][j] = min(substitute, insert, delete)
     return d
 
+
 def find_most_similar_name(fake_name, names):
     lowest_ler = 100.0
     closest_name = ''
@@ -215,20 +90,151 @@ def find_most_similar_name(fake_name, names):
             closest_name = name
     return closest_name.capitalize()
 
+
+class TimeVaryingMarkovLanguageModel:
+    def __init__(self, start_token='<', end_token='>'):
+        self.start_token = start_token
+        self.end_token = end_token
+        self.samples_fit = None
+        self.states = None
+        self.transitions = None
+    
+    def _unique_states(self, samples: List[str]):
+        """Creates a list of unique characters seen in the given names
+        
+        Args:
+            samples (list): A list of strings of tokens to create vocab
+                
+        Returns:
+            list: Unique characters from sample tokens
+        
+        """
+        
+        # Initialize list with our designated start and end tokens
+        states = [self.start_token, self.end_token]
+        
+        # Collect unique characeters
+        for token in samples:
+            for char in list(token):
+                if char not in states:
+                    states.append(char)
+        return states
+    
+    def _new_freq_matrix(self, size):
+        """Creates an empty numpy array used for holding frequency
+            statistics between each character
+            
+        Args:
+            size (int): Size of matrix, i.e. number of unique characters
+                
+        Returns:
+            ndarray: Zero initialized array of shape (size, size)
+        
+        """
+        freq_matrix = np.zeros((size, size))
+        return freq_matrix
+    
+    def fit(self, samples: List[str]):
+        """Calculates and stores the empirical transition probabilites 
+        for every character at every time step in the given dataset
+        
+        Args:
+            samples (list): A list of strings of tokens to compute
+                probabilities from
+        """
+        
+        self.states = self._unique_states(samples)
+        
+        # Get size of the vocab
+        size = len(self.states)
+        transitions = {}
+        
+        for token in samples:
+            # Append the start and end tokens to the name string in order
+            # to easily include them in calculations
+            token = self.start_token + token + self.end_token
+            
+            for i, char in enumerate(token):
+                # We iterate until the end token, since no transition can
+                # be made at the end token
+                if i+1 < len(token):
+                    freq_matrix = transitions.get(i, self._new_freq_matrix(size))
+                    state = self.states.index(char)
+                    next_state = self.states.index(token[i+1])
+                    freq_matrix[state, next_state] += 1
+                    transitions[i] = freq_matrix
+                    
+        for i in transitions.keys():
+            freq_matrix = transitions[i] + 1e-8 # epsilon to avoid div by 0
+            freq_matrix = freq_matrix/freq_matrix.sum(axis=1)[:,None]
+            transitions[i] = freq_matrix
+                    
+        self.transitions = transitions
+        self.samples_fit = samples
+    
+    def generate(self, random=True):
+        """Generate a name give then transition probabilities and
+        unique characters
+        
+        Args:
+            random (bool, optional): If True then each character will be
+                randomly sampled based on the previous character. If False
+                then the name generation will be deterministic by choosing the
+                most frequent letter at each time step based on the last letter
+                
+        Returns:
+            str: The generated token
+        
+        """
+        
+        # Initialize name with the designated start token
+        token = self.start_token
+        current_char = self.start_token
+        
+        for i in self.transitions.keys():
+            # Get the transition probabilities for the current time step
+            prob_matrix = self.transitions[i]
+            
+            # Get the position of the current character
+            state = self.states.index(current_char)
+            
+            # Get the row of transition probs for the current character
+            prob_row = prob_matrix[state]
+            
+            if random:
+                # Randomly sample from P(x|c) to choose the next character
+                new_state = np.random.choice(len(self.states), p=prob_row)
+            else:
+                # Or choose the character with highest frequency
+                new_state = np.argmax(prob_row)
+            
+            # Get the new character string based on the position
+            new_char = self.states[new_state]
+            # Append to the name
+            token += new_char
+            # Reassign current character for next iteration
+            current_char = new_char
+            # Break once the end token is chosen
+            if current_char == self.end_token:
+                break
+            
+        # Strip the name of the start and end tokens
+        return token.strip(self.start_token + self.end_token).capitalize()
+
+
 def main():
-    names_first, names_last = get_player_names(list(range(2010,2020)))
-    vocab = make_vocab(names_first + names_last)
-    transitions_first = transition_probabilities(names_first, vocab)
-    transitions_last = transition_probabilities(names_last, vocab)
+    names_first, names_last = get_player_names(list(range(2019,2020)))
     
-    first = generate_name(transitions_first, vocab)
-    last = generate_name(transitions_last, vocab)
+    first_model = TimeVaryingMarkovLanguageModel()
+    first_model.fit(names_first)
+    last_model = TimeVaryingMarkovLanguageModel()
+    last_model.fit(names_last)
     
-#    closest_first = find_most_similar_name(first, names_first)
-#    closest_last = find_most_similar_name(last, names_last)
-    
+    first = first_model.generate()
+    last = last_model.generate()
+
     print(first, last)
-#    print('Closest real names:', closest_first, closest_last)
+
     
 if __name__ == '__main__':
     main()
